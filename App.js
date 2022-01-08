79bd8ac39267b379853ff0cc45b81bee65399e75
Oseias Beu <oseiasbeu@outlook.com>
2022-01-08 20:34:18 -0300

    Criar Relógio

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React from "react";
+import { StyleSheet, Text, View, ScrollView, TextInput } from "react-native";
+export default class App extends React.Component {
+  //Este método é usado para inicializar nosso componente com estado inicial, nenhum elemento de UI nativo foi renderizado
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date()
+    };
+  }
+tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+ componentDidMount() {
+     this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  componentWillUnmount() {
+      clearInterval(this.timerID);
+  }
+render() {
+    return (
+        <View style={styles.container}>
+          <Text style={styles.clock}>Relógio</Text>
+          <Text style={styles.clock}>{this.state.date.toLocaleTimeString()}</Text>
+        </View>
+      )
+  }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center',
+    width: '100%',
+    backgroundColor: 'black',
+    marginTop: 50
   },
+  clock: {
+    fontSize: 50,
+    color: '#bf740e',
+    marginTop: 50
+  }
 });
